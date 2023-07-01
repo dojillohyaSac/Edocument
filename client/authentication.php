e91c9af75c401fe45eab596746bca5b21c6d742b
dojillohyaSac <dojillohya@sac.edu.ph>
2023-07-01 23:55:46 +0800

    modified text

--- client/authentication.php
+++ client/authentication.php
@@ -2,8 +2,8 @@
 session_start();
 
 if (!isset($_SESSION['authenticated'])) {
-    $_SESSION['status'] = "Please Login to Access the Page";
-    $_SESSION['status_text'] = " ";
+    $_SESSION['status'] = "Denied Access!";
+    $_SESSION['status_text'] = "Please Login to Access the Page";
     $_SESSION['status_code'] = "warning";
     $_SESSION['status_btn'] = "Back";
     header("Location: /eDocument/login");
