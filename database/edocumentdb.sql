811ea184444a813695b9e7c44d1ba5800aec26da
dojillohyaSac <dojillohya@sac.edu.ph>
2023-07-02 17:49:00 +0800

    database modified

--- database/edocumentdb.sql
+++ database/edocumentdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2023 at 02:47 PM
+-- Generation Time: Jul 02, 2023 at 11:48 AM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -49,8 +49,8 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 CREATE TABLE `admin_request` (
   `id` int(11) NOT NULL,
   `DocuCode` varchar(255) NOT NULL,
-  `Sender` varchar(255) NOT NULL,
-  `DocuName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `DocuType` varchar(255) NOT NULL,
   `RequestAction` varchar(255) NOT NULL,
   `Date_of_Request` date NOT NULL DEFAULT current_timestamp(),
   `Status` int(11) NOT NULL DEFAULT 0,
@@ -66,6 +66,7 @@ CREATE TABLE `admin_request` (
 CREATE TABLE `clients_acc` (
   `id` int(11) NOT NULL,
   `cFirstname` varchar(255) NOT NULL,
+  `cMiddlename` varchar(255) NOT NULL,
   `cLastname` varchar(255) NOT NULL,
   `cUsername` varchar(255) NOT NULL,
   `cPassword` varchar(255) NOT NULL,
@@ -73,7 +74,48 @@ CREATE TABLE `clients_acc` (
   `cAddress` varchar(500) NOT NULL,
   `cEmail` varchar(255) NOT NULL,
   `cContactNumber` varchar(255) NOT NULL,
+  `cPicture` varchar(255) NOT NULL,
+  `verifyCode` varchar(255) NOT NULL,
+  `password_token` varchar(255) NOT NULL,
+  `verify_status` int(11) NOT NULL DEFAULT 0 COMMENT '0 = false, 1 = true',
+  `admin_status` int(11) NOT NULL DEFAULT 0,
   `date_created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients_acc`
+--
+
+INSERT INTO `clients_acc` (`id`, `cFirstname`, `cMiddlename`, `cLastname`, `cUsername`, `cPassword`, `cDOB`, `cAddress`, `cEmail`, `cContactNumber`, `cPicture`, `verifyCode`, `password_token`, `verify_status`, `admin_status`, `date_created`) VALUES
+(1, 'Hya', 'Genodepa', 'Dojillo', 'HelloWorld', 'Hello_01', '2000-01-01', 'skajdkajskdj', 'yangyangdojillo01@gmail.com', '09123456789', '2144314.png', '3a3c2d15838785cf6f29902d00efe15d', '', 1, 1, '2023-07-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `Docu_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `Docu_type` varchar(255) NOT NULL,
+  `nso_psa` varchar(255) NOT NULL,
+  `baptismal_cert` int(11) NOT NULL,
+  `marriage_cert_Parents` varchar(255) NOT NULL,
+  `cedula` varchar(255) NOT NULL,
+  `voters_cert` varchar(255) NOT NULL,
+  `birth_cert_Sibling` varchar(255) NOT NULL,
+  `joint_affidavit` varchar(255) NOT NULL,
+  `brgy_cert` varchar(255) NOT NULL,
+  `cenomar_both` varchar(255) NOT NULL,
+  `birth_cert_both` varchar(255) NOT NULL,
+  `tree_planting` varchar(255) NOT NULL,
+  `mar_counseling` varchar(255) NOT NULL,
+  `cert_legal_capacity` varchar(255) NOT NULL,
+  `divorce_paper` varchar(255) NOT NULL,
+  `passport` varchar(255) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -99,6 +141,12 @@ ALTER TABLE `clients_acc`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -118,6 +166,12 @@ ALTER TABLE `admin_request`
 -- AUTO_INCREMENT for table `clients_acc`
 --
 ALTER TABLE `clients_acc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
